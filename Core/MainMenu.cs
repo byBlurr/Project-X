42c42c7c9403e24598e9b94681757bb505ceb17f
byBlurr <dbldumont11@gmail.com>
2026-07-29 09:54:17 +0100

    Added main menu and started enemy entity

---
+++ Core/MainMenu.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+    [Export] private string _gameplayScenePath = "res://Game.tscn";
+
+    public override void _Ready()
+    {
+        GetNode<Button>("MainMenuButtons/PlayButton").Pressed += OnPlayButtonPressed;
+        GetNode<Button>("MainMenuButtons/SettingsButton").Pressed += OnSettingsButtonPressed;
+        GetNode<Button>("MainMenuButtons/ExitButton").Pressed += OnExitButtonPressed;
+    }
+
+    private void OnPlayButtonPressed()
+    {
+        GetTree().ChangeSceneToFile(_gameplayScenePath);
+    }
+
+    private void OnSettingsButtonPressed()
+    {
+        GD.Print("Settings menu opened!");
+    }
+
+    private void OnExitButtonPressed()
+    {
+        GetTree().Quit();
+    }
+}
